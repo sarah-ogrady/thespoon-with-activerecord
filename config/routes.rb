5707a0859bbb2a4116d152fc1c3ced6cb84a7f6a
Sarah OGrady <sarahfrancesogrady@gmail.com>
2021-02-16 09:45:14 +0000

    create controllerr for restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # # read all restaurants
+  # get '/restaurants', to: "restaurants#index"
+  # # read one restaurant
+  # get '/restaurants/:id', to: 'restaurants#show'
+  # # create restaurant
+  # get '/restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurants#create'
+  # # update restaurant
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # patch '/restaurants/:id', to: 'restaurants#update'
+  # # delete restaurant
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+  # # verb + ‘path’, to: ‘controller#action’
+
+  resources :restaurants
 end
