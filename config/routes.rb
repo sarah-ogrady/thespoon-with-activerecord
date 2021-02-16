98a91b73d90a0da1d7e9b5e90852be82b6ace363
Sarah OGrady <sarahfrancesogrady@gmail.com>
2021-02-16 11:36:23 +0000

    upload all code

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # # read all restaurants
+  # # # read all restaurants
   # get '/restaurants', to: "restaurants#index"
-  # # read one restaurant
-  # get '/restaurants/:id', to: 'restaurants#show'
   # # create restaurant
   # get '/restaurants/new', to: 'restaurants#new'
   # post '/restaurants', to: 'restaurants#create'
+  # # read one restaurant
+  # get '/restaurants/:id', to: 'restaurants#show'
   # # update restaurant
   # get '/restaurants/:id/edit', to: 'restaurants#edit'
   # patch '/restaurants/:id', to: 'restaurants#update'
